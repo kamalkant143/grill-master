101e3c664575b2f1efa076744cc9efd3077a4b68
kamal kant <kamalshakya14311@gmail.com>
2024-06-19 08:39:49 +0530

    about section

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector('#about').addEventListener('mousemove', (e) => {
+    const image = document.getElementById('movingImage');
+    const containerRect = e.currentTarget.getBoundingClientRect();
+    const mouseX = e.clientX - containerRect.left;
+    const mouseY = e.clientY - containerRect.top;
+
+    const moveX = (mouseX - containerRect.width / 2) / containerRect.width * 20; // Adjust multiplier for more/less movement
+    const moveY = (mouseY - containerRect.height / 2) / containerRect.height * 20; // Adjust multiplier for more/less movement
+
+    image.style.transform = `translate(${moveX}px, ${moveY}px)`;
+
+});
