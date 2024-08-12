fc42bcb71a8f12410c799898e4482ba1f9b58b2a
kamal kant <kamalshakya14311@gmail.com>
2024-08-12 17:48:29 +0530

    resposive

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
-let openNavBar =document.querySelector(".fa-bars");
-let closeNavBar = document.querySelector(".fa-circle-xmark")
-let navBar =document.querySelector(".nav-bar");
-let list =document.querySelectorAll(".nav-bar li a ")
-// console.log(list);
+let openNavBar = document.querySelector(".fa-bars");
+let closeNavBar = document.querySelector(".fa-circle-xmark");
+let navBar = document.querySelector(".nav-bar");
+let list = document.querySelectorAll(".nav-bar li a ");
 
+openNavBar.addEventListener("click", function () {
+  navBar.classList.toggle("main-nav");
+});
 
-openNavBar.addEventListener("click",function(){
-navBar.classList.toggle("main-nav")
-})
+closeNavBar.addEventListener("click", function () {
+  navBar.classList.remove("main-nav");
+});
 
+window.onscroll = function () {
+  var header = document.querySelector("header");
 
-
-closeNavBar.addEventListener("click",function(){
-    navBar.classList.remove("main-nav")
-    })
-
-
-    window.onscroll = function() {
-        var header= document.querySelector("header");
-    
-        if (window.scrollY> 0) {
-            header.classList.add("fixed");
-        } else {
-            header.classList.remove("fixed");
-        }
-    };
-
-
+  if (window.scrollY > 0) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+};
